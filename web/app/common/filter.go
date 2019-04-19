87233f3d006b9e68f2450163a97d139182dd37d1
qb <745767382@qq.com>
2019-04-19 10:04:47 +0800

    login test 1

--- web/app/common/filter.go
+++ web/app/common/filter.go
@@ -38,7 +38,7 @@ const (
 	VendorsPath               = "/vendors"
 	DataPathPrefix            = "/data"
 	StaticDirName             = "test"
-	LoginUriPath              = "/api/v1/authan/login"
+	LoginUriPath              = "/api/v1/auth/login"
 	LoginHtmlPage             = "/login.html"
 	NoAuthorizationMsg        = "no authorization."
 	AjaxRequestIdentifier     = "XMLHttpRequest"
